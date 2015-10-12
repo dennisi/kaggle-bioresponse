aed8eae0134b15a270ddf0026fc3a0daf96adc18
dennisi <ignatenko@gmail.com>
2015-10-11 21:56:43 -0500

    corrected folder name
    changed case from 'Data' to 'data'

--- makeSubmission.py
+++ makeSubmission.py
@@ -3,10 +3,10 @@ from numpy import genfromtxt, savetxt
 
 def main():
     #create the training & test sets, skipping the header row with [1:]
-    dataset = genfromtxt(open('Data/train.csv','r'), delimiter=',', dtype='f8')[1:]    
+    dataset = genfromtxt(open('data/train.csv','r'), delimiter=',', dtype='f8')[1:]    
     target = [x[0] for x in dataset]
     train = [x[1:] for x in dataset]
-    test = genfromtxt(open('Data/test.csv','r'), delimiter=',', dtype='f8')[1:]
+    test = genfromtxt(open('data/test.csv','r'), delimiter=',', dtype='f8')[1:]
 
     #create and train the random forest
     #multi-core CPUs can use: rf = RandomForestClassifier(n_estimators=100, n_jobs=2)
@@ -14,7 +14,7 @@ def main():
     rf.fit(train, target)
     predicted_probs = [[index + 1, x[1]] for index, x in enumerate(rf.predict_proba(test))]
 
-    savetxt('Data/submission.csv', predicted_probs, delimiter=',', fmt='%d,%f', 
+    savetxt('data/submission.csv', predicted_probs, delimiter=',', fmt='%d,%f', 
             header='MoleculeId,PredictedProbability', comments = '')
 
 if __name__=="__main__":
